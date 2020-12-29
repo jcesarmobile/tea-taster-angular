3e8e13525c182168501f75cc5dec1f3f0095b7c2
Ken Sodemann <ken@ionic.io>
2020-12-29 16:19:21 -0600

    step(eleven): get the tea data

---
+++ src/app/core/tea/tea.service.mock.ts
@@ -0,0 +1,8 @@
+import { EMPTY } from 'rxjs';
+import { TeaService } from './tea.service';
+
+export function createTeaServiceMock() {
+  return jasmine.createSpyObj<TeaService>('TeaService', {
+    getAll: EMPTY,
+  });
+}
