c23a7507171ebf15956cc13bc1dad7021ead3043
Ken Sodemann <ken@ionic.io>
2021-01-01 16:04:35 -0600

    step(four): add custom PIN dialog

--- src/app/core/session-vault/session-vault.service.spec.ts
+++ src/app/core/session-vault/session-vault.service.spec.ts
@@ -1,17 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 import { Store } from '@ngrx/store';
-import { Platform } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
+
+import { SessionVaultService } from './session-vault.service';
 import { provideMockStore } from '@ngrx/store/testing';
-import { createPlatformMock } from '@test/mocks';
+import {
+  createOverlayControllerMock,
+  createOverlayElementMock,
+  createPlatformMock,
+} from '@test/mocks';
 import { sessionLocked, sessionRestored } from '@app/store/actions';
+import { PinDialogComponent } from '@app/pin-dialog/pin-dialog.component';
 
 describe('SessionVaultService', () => {
+  let modal: HTMLIonModalElement;
   let service: SessionVaultService;
 
   beforeEach(() => {
+    modal = createOverlayElementMock('Modal');
     TestBed.configureTestingModule({
       providers: [
         provideMockStore(),
+        {
+          provide: ModalController,
+          useValue: createOverlayControllerMock('ModalController', modal),
+        },
         { provide: Platform, useFactory: createPlatformMock },
       ],
     });
@@ -45,5 +58,44 @@ describe('SessionVaultService', () => {
     service.onSessionRestored(session);
     expect(store.dispatch).toHaveBeenCalledTimes(1);
     expect(store.dispatch).toHaveBeenCalledWith(sessionRestored({ session }));
+  });
+
+  describe('onPasscodeRequest', () => {
+    beforeEach(() => {
+      (modal.onDidDismiss as any).and.returnValue(
+        Promise.resolve({ role: 'cancel' }),
+      );
+    });
+
+    [true, false].forEach(setPasscode => {
+      it(`creates a PIN dialog, setting passcode: ${setPasscode}`, async () => {
+        const modalController = TestBed.inject(ModalController);
+        await service.onPasscodeRequest(setPasscode);
+        expect(modalController.create).toHaveBeenCalledTimes(1);
+        expect(modalController.create).toHaveBeenCalledWith({
+          backdropDismiss: false,
+          component: PinDialogComponent,
+          componentProps: {
+            setPasscodeMode: setPasscode,
+          },
+        });
+      });
+    });
+
+    it('presents the modal', async () => {
+      await service.onPasscodeRequest(false);
+      expect(modal.present).toHaveBeenCalledTimes(1);
+    });
+
+    it('resolves to the PIN', async () => {
+      (modal.onDidDismiss as any).and.returnValue(
+        Promise.resolve({ data: '4203', role: 'OK' }),
+      );
+      expect(await service.onPasscodeRequest(true)).toEqual('4203');
+    });
+
+    it('resolves to an empty string if the PIN is undefined', async () => {
+      expect(await service.onPasscodeRequest(true)).toEqual('');
+    });
   });
 });
